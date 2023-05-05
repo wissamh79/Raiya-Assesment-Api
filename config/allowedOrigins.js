b11c151ea7e0f1c2c5c7f84ecec16c1793c024c4
wissamh79 <wsamh797@gmail.com>
2023-05-06 00:39:53 +0300

    adding client-site to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:3000",
   "http://localhost:5173",
+  "https://raiya-assesment.onrender.com",
 ];
 module.exports = allowedOrigins;
